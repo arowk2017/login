a8990e3982cb2b91f8f82aa446b21c669a3688bf
Ken Harlan <arowk2017@gmail.com>
2017-10-21 17:30:07 -0700

    update

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors({credentials: true, origin: 'localhost:4200'}));
 //////////////////////////////
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200/');
+    res.header('Access-Control-Allow-Origin', req.header("Origin"));
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
